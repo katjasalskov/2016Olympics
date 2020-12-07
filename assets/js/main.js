a2c1db8eb7f287ee1c054665430a8ba04cb673b8
katjasalskov <62789103+katjasalskov@users.noreply.github.com>
2020-12-07 12:36:28 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -266,7 +266,7 @@ form.addEventListener("submit", function(event) {
   var data = new FormData(form);
   var output = "";
   for (const entry of data) {
-    output = output + entry[1]  "\r";
+   	output = output + entry[0] + "=" + entry[1] + "\r";
   };
   log.innerText = output;
   event.preventDefault();
